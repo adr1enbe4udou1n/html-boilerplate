2870bfb0774132267dda9929b368b3ce1a98335f
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-01-28 15:51:55 +0100

    up

--- webpack.config.js
+++ webpack.config.js
@@ -49,24 +49,19 @@ module.exports = {
               loader: 'css-loader',
               options: {
                 minimize: production,
-                sourceMap: true,
-                importLoaders: 1
+                sourceMap: true
               }
             }, {
               loader: 'postcss-loader',
               options: {
                 ident: 'postcss',
-                sourceMap: true,
-                plugins: [
-                  require('autoprefixer')
-                ]
+                sourceMap: true
               }
             }, {
-              loader: 'resolve-url-loader?sourceMap'
+              loader: 'resolve-url-loader'
             }, {
               loader: 'sass-loader',
               options: {
-                precision: 8,
                 outputStyle: 'expanded',
                 sourceMap: true
               }
@@ -196,6 +191,7 @@ if (production) {
       parallel: true,
       sourceMap: true
     }),
+    new webpack.HashedModuleIdsPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
